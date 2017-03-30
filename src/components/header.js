653532075d02b643403753eafe1802abba17b95e
alexkarevoll <alex.karevoll@gmail.com>
2017-03-30 12:45:04 -0700

    basic header with basic styles

---
+++ src/components/header.js
@@ -0,0 +1,19 @@
+// Import libraries for making a Component
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a Component
+const Header = () => {
+  const { textStyle } = styles;
+
+  return <Text style={textStyle}>Albums</Text>;
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20
+  }
+};
+
+// Make the component available to other parts of the app
+export default Header;
